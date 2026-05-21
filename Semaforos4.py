98e0f4aa9def865d63b1ff6da66944ed30333474
Nathália Rahy Queiroz <nathaliaryqz28@gmail.com>
2026-05-21 17:08:17 -0300

    Add files via upload

--- Semaforos4.py
+++ Semaforos4.py
@@ -4,41 +4,66 @@ import time
 
 
 semaforo = None
-def init(s):
-    global semaforo
-    semaforo = s
+equipes = None
 
-def voltas(equipe, carro):
+def init(s, eq):
     global semaforo
-    tempo: int = 0
-    voltas: int = 1 
+    global equipes
+    semaforo = s
+    equipes = eq
     
 
 
+def pista(equipe, carro):
+    global semaforo
+    global equipes 
+    voltas: int = 1
+    tempo: int = 0
+
+
+        
+    
+
     with semaforo:
-        while voltas <= 3:
-            tempo = random.randint(1, 3)
+        while equipe in equipes:
+            time.sleep(0.2)
+
+
+        for i in range (5):
+            if equipes[i] == 0:
+                equipes[i] = equipe
+                break
+            
+
+        while voltas<=3:
+            tempo = random.randint(1,3)
             time.sleep(tempo)
             print ('O', carro, 'da equipe', equipe, 'levou', tempo, 'minutos na volta', voltas )
             voltas +=1
 
+        for i in range (5):
+            if equipes[i] == equipe:
+                equipes[i] = 0
+                break
 
 def main():
     sem = None
-    params = [(0, 0)] *14
+    equipes: int = [0]*5
+    equipes = multiprocessing.Array('i', equipes)
+    params = [(0,0)] *14
 
     for i in range (14):
         equipe = (i//2) + 1
-        params[i] = (equipe, 'carroA')
+        params[i] = (equipe, 'Carro_A')
         if i%2 != 0:
-            params[i] = (equipe, 'CarroB')
+            params[i] = (equipe, 'Carro_B')
     
-    print (params)
+    
 
     with multiprocessing.Manager() as manager:
         sem = manager.Semaphore(5)
-        with multiprocessing.Pool(processes=14, initializer=init, initargs=(sem,)) as pool:
-            pool.starmap(voltas, params)
+        with multiprocessing.Pool(processes=14, initializer=init, initargs=(sem, equipes,)) as pool:
+            pool.starmap(pista, params)
 
     
 
@@ -46,3 +71,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
